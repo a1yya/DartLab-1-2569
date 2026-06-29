4be12976324fedde5102aea812240ce2d7feed3e
a1yya <chonladan67@nu.ac.th>
2026-06-29 12:03:38 +0700

    Initial commit

---
+++ lab2_exercise3.dart
@@ -0,0 +1,30 @@
+void main() {
+
+  List<String> subjects = [
+    'Math',
+    'Computer',
+    'Intro to Programming'
+  ];
+
+  print('Number of subjects: ${subjects.length}');
+  print('First subject: ${subjects[0]}');
+  print('Last subject: ${subjects[subjects.length - 1]}');
+
+  subjects.add('English');
+  print('Updated subjects: $subjects');
+
+  print('---');
+
+  Map<String, int> studentScores = {
+    'Math': 68,
+    'Computer': 87,
+  };
+
+  print('Score for Physics: ${studentScores['Computer']}');
+
+  studentScores['Intro to Programming'] = 90;
+
+  print('Updated scores: $studentScores');
+  print('All subjects in map: ${studentScores.keys}');
+  print('All scores in map: ${studentScores.values}');
+}
